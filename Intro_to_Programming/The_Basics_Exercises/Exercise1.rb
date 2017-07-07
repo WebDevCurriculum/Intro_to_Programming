8a8ebad7e34fd6a813733156b2f2964cac8edb8b
Charity <Youngblood.charity@gmail.com>
2017-07-06 19:34:27 -0700

    created variable first_name and variable last_name for exercise1.rb

--- Intro_to_Programming/The_Basics_Exercises/Exercise1.rb
+++ Intro_to_Programming/The_Basics_Exercises/Exercise1.rb
@@ -3,3 +3,8 @@ Add two strings together that,
 when concatenated, return your first and 
 last name as your full name in one string. 
 =end
+
+first_name = "Charity";
+last_name = "Youngblood";
+
+puts first_name + last_name 
