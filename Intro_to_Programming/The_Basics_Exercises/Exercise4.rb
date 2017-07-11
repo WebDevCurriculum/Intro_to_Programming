e70e834bd2b3e43f539336ee24c6d8fd2d826c29
Charity <Youngblood.charity@gmail.com>
2017-07-11 14:18:58 -0700

    added movie titles and dates from Exercise 3 to comments in Exercise4.rb

--- Intro_to_Programming/The_Basics_Exercises/Exercise4.rb
+++ Intro_to_Programming/The_Basics_Exercises/Exercise4.rb
@@ -4,6 +4,16 @@ Use the dates from the previous example
 and store them in an array. Then make your 
 program output the same thing as exercise 3.
 
+Movie Titles and Dates from Exercise 3 
 
-
+The Babadook - 2014
+The Shining - 1980 
+The Blair Witch Project - 1999
+The Conjuring - 2013 
+The Exorcist - 1973
+Psycho - 1960 
+Insidious - 2010 
+What Lies Beneath - 2000
+The Ring - 2002
+The Amityville Horror - 2005
 =end 
