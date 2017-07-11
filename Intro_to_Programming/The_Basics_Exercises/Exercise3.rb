305b1f089050ace68eaa4a984c4e6ae191241f19
Charity <Youngblood.charity@gmail.com>
2017-07-10 18:58:02 -0700

    added favorite horror movie titles to comments in Exercise3.rb

--- Intro_to_Programming/The_Basics_Exercises/Exercise3.rb
+++ Intro_to_Programming/The_Basics_Exercises/Exercise3.rb
@@ -8,4 +8,16 @@ print out the year of each movie to the screen.
 2. locate year movie was made 
 3. enter in format movie titles = {"movie1" => year, "movie2" => year, "movie3" => etc}
 
+Movies: 
+
+The Babadook - 2014
+The Shining - 1980 
+The Blair Witch Project - 1999
+The Conjuring - 2013 
+The Exorcist - 1973
+Psycho - 1960 
+Insidious - 2010 
+What Lies Beneath - 2000
+The Ring - 2002
+The Amityville Horror - 2005
 =end 
