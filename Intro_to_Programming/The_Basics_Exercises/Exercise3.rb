109ccfc9fd4cca29289364620a98775fb4b6ba8e
Charity <Youngblood.charity@gmail.com>
2017-07-10 17:58:06 -0700

    added steps to create hash with movie titles and years to Exercise3.rb

--- Intro_to_Programming/The_Basics_Exercises/Exercise3.rb
+++ Intro_to_Programming/The_Basics_Exercises/Exercise3.rb
@@ -3,4 +3,8 @@ Write a program that uses a hash to store
 a list of movie titles with the year they came out. 
 Then use the puts command to make your program 
 print out the year of each movie to the screen. 
+
+1. locate movie titles
+2. locate year movie was made 
+3. enter in format movie titles = {"movie1" => year, "movie2" => year, "movie3" => etc}
 =end 
