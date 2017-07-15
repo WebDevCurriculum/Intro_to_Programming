b578f28648bf880b25d425c94ef8dccce7497819
Charity <Youngblood.charity@gmail.com>
2017-07-14 18:27:29 -0700

    added steps for program creation in age.rb

--- Intro_to_Programming/Variables_Exercises/age.rb
+++ Intro_to_Programming/Variables_Exercises/age.rb
@@ -2,4 +2,9 @@
 Write a program called age.rb that asks a user 
 how old they are and then tells them how old they will 
 be in 10, 20, 30 and 40 years. 
+
+1. use puts to prompt user for age  
+2. place user input in a variable called user_age
+3. create formula for user_age + 10, etc
+4. puts formula on screen for user 
 =end
