1db8adbac3203a97891d716a11b7de0f873be65c
Charity <Youngblood.charity@gmail.com>
2017-07-14 18:18:24 -0700

    added puts method with message for user input. Added puts to display user input and a greeting

--- Intro_to_Programming/Variables_Exercises/name.rb
+++ Intro_to_Programming/Variables_Exercises/name.rb
@@ -3,3 +3,8 @@ Write a program called name.rb that
 asks the user to type in their name and 
 then prints out a greeting message with their name included.
 =end 
+
+puts "Please type your name: "
+user_name = gets.chomp
+ 
+puts "Hello " + user_name + ". It's nice to meet you!"
