4b6fe7ac52f893ea0d83e8a8da87777f052146dc
Charity <Youngblood.charity@gmail.com>
2017-08-01 14:16:10 -0700

    addedcode from name.rb for modifications listed in instructions

--- Intro_to_Programming/Variables_Exercises/modify_name.rb
+++ Intro_to_Programming/Variables_Exercises/modify_name.rb
@@ -6,3 +6,8 @@ variable, and then does the same for the last name.
 Then outputs their full name all at once.
 
 =end
+
+puts "Please type your name: "
+user_name = gets.chomp
+ 
+puts "Hello " + user_name + ". It's nice to meet you!"
