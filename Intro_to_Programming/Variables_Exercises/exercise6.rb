9b5368edfb474c200839ff6c7ed0b7c0462f9a1c
Charity <Youngblood.charity@gmail.com>
2017-08-01 14:36:19 -0700

    addedanswers to exercise6.rb for how to interpret error message

--- Intro_to_Programming/Variables_Exercises/exercise6.rb
+++ Intro_to_Programming/Variables_Exercises/exercise6.rb
@@ -7,3 +7,8 @@ NameError: undefined local variable or method `shoes' for main:Object
   from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
 
 =end
+
+The error message is displaying the following
+- the variable or method "shoes" has not been defined 
+- the location of the file
+- the row of code the error appears on
