a08f105dd6c7730d1a60c065bc2e6ef3be75087f
Charity <Youngblood.charity@gmail.com>
2017-08-01 14:18:00 -0700

    modified puts method to ask user to input their first name then their last name

--- Intro_to_Programming/Variables_Exercises/modify_name.rb
+++ Intro_to_Programming/Variables_Exercises/modify_name.rb
@@ -7,7 +7,10 @@ Then outputs their full name all at once.
 
 =end
 
-puts "Please type your name: "
-user_name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp
+
+puts "What is your last name? "
+last_name = gets.chomp
  
 puts "Hello " + user_name + ". It's nice to meet you!"
