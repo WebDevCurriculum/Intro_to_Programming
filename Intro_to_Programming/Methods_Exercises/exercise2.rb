ee377b976e3de654c360d0dd1c6871e647c8ddb9
Charity <Youngblood.charity@gmail.com>
2017-08-01 18:56:19 -0700

    added answers to questions in exercise2.rb

--- Intro_to_Programming/Methods_Exercises/exercise2.rb
+++ Intro_to_Programming/Methods_Exercises/exercise2.rb
@@ -13,3 +13,9 @@
   5. print something = "nothing"
 
 =end
+
+1. This is read as "2 is assigned to x"
+2. This will print out "x = 2" on the screen
+3. This will print out " "name = "Joe" "
+4. Nothing 
+5. This will print " "something = "nothing" "
