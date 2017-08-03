025b5930097a000129dc19d2c9613ca72aa0546c
Charity <Youngblood.charity@gmail.com>
2017-08-02 18:24:09 -0700

    added code from exercise4.rb, deleted return keyword

---
+++ Intro_to_Programming/Methods_Exercises/exercise5.rb
@@ -0,0 +1,22 @@
+=begin 
+
+1) Edit the method definition in exercise #4 
+so that it does print words on the screen. 
+2) What does it return now?
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+=end 
+
+  def scream(words)
+    words = words + "!!!!"
+    puts words
+end
+
+scream("Yippeee")
