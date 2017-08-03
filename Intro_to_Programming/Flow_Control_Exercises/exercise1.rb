d773ebdf904d959fbea0c670fab54475303fbbc4
Charity <Youngblood.charity@gmail.com>
2017-08-02 19:08:02 -0700

    added conditional statements from exercise with answers to what they would return

--- Intro_to_Programming/Flow_Control_Exercises/exercise1.rb
+++ Intro_to_Programming/Flow_Control_Exercises/exercise1.rb
@@ -7,7 +7,16 @@ into irb to see the results.
 =end 
 
 1. (32 * 4) >= 129
+Will return false
+
 2. false != !true
+Will return false
+
 3. true == 4
+Will return false 
+
 4. false == (847 == '874')
+Will return true
+
 5. (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+    false          false                   true          false
